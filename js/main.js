cb7abf675f7b65a3eb191728e0ff696e92366ce2
Ethan Bellora <Belloramail@gmail.com>
2017-05-25 10:18:00 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,138 @@
+window.onload = (function () {
+    const articleBtn = document.getElementById('articles'),
+            aboutBtn = document.getElementById('about'),
+          contactBtn = document.getElementById('contact'),
+          navigation = document.querySelector('.navi'),
+            art_list = document.getElementById('test').children;
+            // art_list = document.getElementsByClassName('colorful');
+
+    //array of all hr's
+    const hr_array = document.getElementsByTagName('hr');
+
+    aboutBtn.onmouseover = function () {
+        navigation.style.background = 'var(--cool_blue)';
+        for (let i = 0; i < hr_array.length; i++) {
+            hr_array[i].style.borderBottomColor = '#4F636F';
+        }
+        for (let z = 0; z < art_list.length; z++) {
+            art_list[z].style.background = 'var(--cool_blue)'
+        }
+        title.style.color = '#4F636F';
+    }
+    contactBtn.onmouseover = function () {
+        // navigation.style.background = '#009688';
+        navigation.style.background = '#009688';
+        for (let i = 0; i < hr_array.length; i++) {
+            // hr_array[i].style.borderBottomColor = '#074943';
+            hr_array[i].style.borderBottomColor = '#074943';
+        }
+        for (let z = 0; z < art_list.length; z++) {
+            art_list[z].style.background = '#009688'
+        }
+        // title.style.color = '#074943';
+        title.style.color = '#074943';
+    }
+    articleBtn.onmouseover = function () {
+        navigation.style.background = 'rosybrown';
+        for (let i = 0; i < hr_array.length; i++) {
+            hr_array[i].style.borderBottomColor = 'var(--dark_brown)';
+        }
+        for (let z = 0; z < art_list.length; z++) {
+            art_list[z].style.background = 'rosybrown'
+        }
+        title.style.color = 'var(--dark_brown)';
+    }
+    articleBtn.onclick = function () {
+        //base transition duration will be .3s and increment by .2s
+        let delay = .3;
+
+        //for the length of the items in the list module
+        for (let i = 0; i < test.children.length; i++) {
+            let current = test.children[i];
+            
+            //Set the transition properties using the delay variable
+            current.style.transition = `all ${delay}s ease-out`;
+            
+            //toggle the class to hide or show the current list item and increment the delay before moving along
+            current.classList.toggle('bar-hide');
+            current.classList.toggle('bar-show');
+            delay += .2;
+        } 
+    };
+    scroll_art.onclick = function () {
+        //base transition duration will be .3s and increment by .2s
+        let delay = .3;
+
+        //scroll up first
+        document.getElementsByClassName('navi')[0].scrollIntoView({
+            behavior: 'smooth'
+        })
+        //for the length of the items in the list module
+        for (let i = 0; i < test.children.length; i++) {
+            let current = test.children[i];
+            
+            //Set the transition properties using the delay variable
+            current.style.transition = `all ${delay}s ease-out`;
+            
+            //toggle the class to hide or show the current list item and increment the delay before moving along
+            current.classList.remove('bar-hide');
+            current.classList.add('bar-show');
+            delay += .2;
+        } 
+    };
+    scroll_abt.onclick = function () {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        });
+
+        document.getElementsByClassName('about')[0].classList.remove('about-down');
+        document.getElementsByClassName('about')[0].classList.add('about-up')
+    };
+    scroll_cnt.onclick = function () {
+        window.scroll({
+            top: 771,
+            behavior: 'smooth'
+        });
+
+        cnt.classList.add('contact-show');
+        cnt.classList.remove('contact-hide');
+    }
+    contactBtn.onclick = function () {
+        window.scroll({
+            top: 771,
+            behavior: 'smooth'
+        });
+
+        cnt.classList.toggle('contact-show');
+        cnt.classList.toggle('contact-hide');
+    };
+    aboutBtn.onclick = function () {
+        document.getElementsByClassName('about')[0].classList.toggle('about-down');
+        document.getElementsByClassName('about')[0].classList.toggle('about-up')
+    };
+    
+
+
+    //Below are helper functions
+    //---------------------------------------------------------------------------
+    //helper resources: (const vs let: http://wesbos.com/let-vs-const/), ()
+    //true equals capturing, false is bubbling
+    const body = document.getElementsByTagName('body')[0];
+    const html = document.getElementsByTagName('html')[0];
+    
+    body.addEventListener('click', function(data) {
+        console.log('parent data: ', data);
+    });
+    window.onscroll = function(e) {
+        let bounds = navigation.getBoundingClientRect();
+        console.log(bounds.top);
+        if (bounds.top < 0) {
+            scrollbar.classList.remove('scroll-hide');
+            scrollbar.classList.add('scroll-show');
+        } else if (bounds.top > 0) {
+           scrollbar.classList.add('scroll-hide');
+           scrollbar.classList.remove('scroll-show');
+        }
+    }
+})();
